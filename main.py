22f249eb9591f831bd77242efbb70cd3d91dee9f
quietkiller4 <kevinkellerjr@gmail.com>
2024-03-10 16:09:54 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def cross_the_bridge():  # 2 people on the left side go to the right side
+    pass
+
+def person_come_back():  # 1 person from the right side go back to the left side
+    pass
+
+def minium_time(times):
+    pass
+
+times = [1, 2, 5, 10]
+min_time = minium_time(times)
+print(min_time)
