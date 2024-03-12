b2fdfe971726edd41b98033823428d570fb107fc
quietkiller4 <kevinkellerjr@gmail.com>
2024-03-12 18:26:52 -0400

    got farther

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import random
 
 
-def cross_the_bridge(times):  # 2 people on the left side go to the right side
-    #randomly going to pick 2 people to cross the bridge
-    x = 0
-    y = 6
-    randomize = random.randint(x, y)
-    print(randomize)
-    return 5
+def cross_the_bridge(times, time_it_should_be):  # 2 people on the left side go to the right side
+    right_side = 0
+    left_side = len(times)
+    print(left_side)
+
 
 def person_come_back(times):  # 1 person from the right side go back to the left side
     pass
@@ -20,11 +18,13 @@ def minium_time(times):
     else:
         return 22
 
+
 times = [1, 2, 5, 10]
 min_time = minium_time(times)
 print(min_time)
-done = True
-while done:
-    time = cross_the_bridge(times)
-    if time <= min_time:
-        done = False
+#done = True
+#while done:
+time = cross_the_bridge(times, min_time)
+    #done = False
+    #if time <= min_time:
+        #done = False
